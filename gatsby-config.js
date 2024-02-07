9f0e090261ae684dcc741fe6f8c2eda8fb4178dc
Johann Taylor <j.taylor1343@gmail.com>
2024-02-07 14:59:47 -0500

    Updated dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@
  */
 
  module.exports = {
-  pathPrefix: "/utppp",
+  pathPrefix: "/utpppV2",
   siteMetadata: {
     title: "University of Toronto Piano Pedagogy Program",
     description: "Piano Pedagogy at the University of Toronto's Music Faculty"
